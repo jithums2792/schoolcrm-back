057cb14819c98d02e9e8676f0cea7863dc4b6a3b
jithums2792 <greenox2792@gmail.com>
2020-05-31 13:59:16 +0530

    jj

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http, {
     handlePreflightRequest: (req, res) => {
         const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
+            "Access-Control-Allow-Origin": 'http://localhost:4200',
         };
         res.writeHead(200, headers);
         res.end();
