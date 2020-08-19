f77a279096dd4f475cf9c08b618d977ca0a946d4
jithums2792 <greenox2792@gmail.com>
2020-08-19 10:22:49 +0530

    new changes

---
+++ controllers/subexam.controller.js
@@ -0,0 +1,21 @@
+const examModel = require('../models/subexam.model')
+
+function dataHandler(res, data) {res.json({status: 'success', data: data})}
+function errorHandler(res, data) {res.json({status: 'error', data: data})}
+function getAllexam(res) {examModel.find().then(data => dataHandler(res, data)).catch(err => errorHandler(res,err))}
+function getexamById(req,res) {examModel.findById(req.params.id).then(data => dataHandler(res, data)).catch(err => errorHandler(res, err))}
+function updateexamById(req,res) { examModel.findByIdAndUpdate(req.params.id, req.body, {new: true}).then(data => dataHandler(res, data)).catch(err => errorHandler(res, err))}
+function deleteexamById(req,res) {examModel.findByIdAndDelete(req.params.id).then(data => dataHandler(res,data)).catch(err => errorHandler(res, err))}
+function getExamBycategory(req,res) {examModel.find(req.body).then(data => dataHandler(res, data)).catch(err => errorHandler(res, err))}
+function addexam(req,res) {
+    let newexam = new examModel(req.body)
+    newexam.save().then(data => dataHandler(res, data)).catch(err => errorHandler(res, err));
+}
+
+
+exports.getAllexam = getAllexam
+exports.getexamById = getexamById
+exports.updateexamById = updateexamById
+exports.deleteexamById = deleteexamById
+exports.addexam = addexam
+exports.getExamBycategory = getExamBycategory
---
+++ models/subexam.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const schema = mongoose.Schema;
+const exam = new schema({
+   name: {type: String, required: true},
+   class: {type: String, required: true},
+   section: {type: String, required: true},
+   subject: {type: String, required: true},
+   teacher: {type: String, required: true},
+   startdate: {type: String},
+   enddate: {type: String},
+   totalmarks: {type: Number, required: true},
+   questionlist: {type: Array, required: true},
+   active:{type: Boolean}
+})
+const subexammodel = mongoose.model('subexam', exam)
+module.exports = subexammodel
